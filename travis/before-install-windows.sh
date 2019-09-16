fa9631d7e3fba28097a18b12191cb34ff7dca5f2
njlr <njlr@users.noreply.github.com>
2019-09-16 16:05:31 +0100

    * Tweaks scripts

--- travis/before-install-windows.sh
+++ travis/before-install-windows.sh
@@ -2,8 +2,6 @@
 
 choco install buckaroo
 
-export MSYS2_ARG_CONV_EXCL="//"
-
 if [ $BUCKAROO_USE_BAZEL ]
 then
 
@@ -15,7 +13,6 @@ choco install zip
 choco install unzip
 choco install buck
 
-powershell -Command 'refreshenv'
 powershell -Command 'set-executionpolicy unrestricted'
 powershell -Command 'Install-Module -Name PSCX -AllowClobber'
 powershell -Command 'Install-Module -Name VSSetup -AllowClobber'
