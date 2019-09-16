c3b91912758414bf28cc66a70f2c13649c4a9f5b
njlr <njlr@users.noreply.github.com>
2019-09-16 15:26:47 +0100

    * Tweaks Windows

--- travis/before-install-windows.sh
+++ travis/before-install-windows.sh
@@ -9,8 +9,11 @@ choco install bazel
 
 else
 
+choco install zip
+choco install unzip
 choco install buck
 
+ps 'refreshenv'
 ps 'set-executionpolicy unrestricted'
 ps 'Install-Module -Name PSCX -AllowClobber'
 ps 'Install-Module -Name VSSetup -AllowClobber'
